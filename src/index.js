ff19e4b82b5fc0732cd26648b049f4be4ea3a601
victornpb <3372598+victornpb@users.noreply.github.com>
2022-05-02 19:30:50 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@
  * }
  */
 
- const PLUGIN_NAME = 'rollup-plugin-baked-env';
- import { dataToEsm } from '@rollup/pluginutils';
- 
- /**
+const PLUGIN_NAME = 'rollup-plugin-baked-env';
+import { dataToEsm } from '@rollup/pluginutils';
+
+/**
   * It takes an object and returns a Rollup plugin that will create a module that exports the object as
   * a JavaScript module
   * @param {object} [variables=process.env] The variables to be exported. Defaults to process.env.
@@ -23,42 +23,42 @@
   * @param {string} [options.indent] The indentation to use.
   * @returns A function that takes two arguments, variables and options.
   */
- export default function envPlugin(variables = process.env, options = {}) {
- 
-   let moduleName = options.moduleName ?? 'process.env';
-   let indent = 'indent' in options ? options.indent : '\t';
- 
-   return {
-     name: PLUGIN_NAME,
-     resolveId(id) {
-       if (id === moduleName) {
-         return id;
-       }
-     },
-     load(id) {
-       if (id === moduleName) {
-         let code = dataToEsm(variables, {
-           namedExports: true,
-           objectShorthand: true, // irrelevant as we don't use the default export
-           // disableDefaultExport: true, // this option doesn't exist
- 
-           // code generation preferences
-           preferConst: options.preferConst ?? true,
-           compact: options.compact,
-           indent: indent,
-         });
- 
-         // remove default export from code so people can't import the whole process.env object
-         // because it would embedd all the environment variables in the code,
-         // this could lead to unintentionally which could expose sensitive information
-         // replace with a empty object
-         // code = code.replace(/export default[\s\S]*$/, '');
- 
-         return {
-           code: `/* ${PLUGIN_NAME} */` + `\n${code}\n`,
-           map: { mappings: '' }
-         };
-       }
-     },
-   };
- }
+export default function envPlugin(variables = process.env, options = {}) {
+
+  let moduleName = options.moduleName ?? 'process.env';
+  let indent = 'indent' in options ? options.indent : '\t';
+
+  return {
+    name: PLUGIN_NAME,
+    resolveId(id) {
+      if (id === moduleName) {
+        return id;
+      }
+    },
+    load(id) {
+      if (id === moduleName) {
+        let code = dataToEsm(variables, {
+          namedExports: true,
+          objectShorthand: true, // irrelevant as we don't use the default export
+          // disableDefaultExport: true, // this option doesn't exist
+
+          // code generation preferences
+          preferConst: options.preferConst ?? true,
+          compact: options.compact,
+          indent: indent,
+        });
+
+        // remove default export from code so people can't import the whole process.env object
+        // because it would embedd all the environment variables in the code,
+        // this could lead to unintentionally which could expose sensitive information
+        // replace with a empty object
+        // code = code.replace(/export default[\s\S]*$/, '');
+
+        return {
+          code: `/* ${PLUGIN_NAME} */` + `\n${code}\n`,
+          map: { mappings: '' }
+        };
+      }
+    },
+  };
+}
